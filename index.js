7e92f24b7cd6637af40b3f7a0b6ab93132ed3067
Md Rashed Ali <tools.rashed@gmail.com>
2023-08-28 18:57:12 +0600

    modal done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const phoneToDisplay = () => {
 };
 
 const runWebsite = (isShowMore) => {
-    loading(true);
+  loading(true);
   let inputField = document.querySelector(".input").value;
   if (inputField === "") {
     getProducts((inputField = "a"), isShowMore);
@@ -40,7 +40,7 @@ const setProducts = (phoneData, isShowMore) => {
   for (phone of phoneData) {
     const phoneBrand = phone.brand;
     const phoneName = phone.phone_name;
-
+    const productId = phone.slug;
     const phoneImageRaw = phone.image;
     const phoneImage = `src="${phoneImageRaw}"`;
 
@@ -51,9 +51,9 @@ const setProducts = (phoneData, isShowMore) => {
    
           <div class="phone-card bg-slate-200 p-4 rounded-xl space-y-2 flex justify-center items-center flex-col">
             <img class="rounded-xl w-20 h-30" ${phoneImage} alt="">
-            <h2>${phoneName}</h2>
-            <h3>Brand: ${phoneBrand} </h3>
-
+            <h2 class="text-2xl">${phoneName}</h2>
+            <h3 class="text-xl" >Brand: ${phoneBrand} </h3>
+            <button class="btn" onclick="showDetails('${productId}')">Show Details</button>
           </div>
         `;
     phoneContainer.appendChild(phoneCard);
@@ -61,16 +61,66 @@ const setProducts = (phoneData, isShowMore) => {
   loading(false);
 };
 
-
 const loading = (isLoading) => {
-    const loadingScreen = document.getElementById("loading-screen");
-console.log(loadingScreen)
-    if (isLoading) {
-        loadingScreen.classList.remove("hidden");
-    } else {
-        loadingScreen.classList.add("hidden");
-    }
+  const loadingScreen = document.getElementById("loading-screen");
+  console.log(loadingScreen);
+  if (isLoading) {
+    loadingScreen.classList.remove("hidden");
+  } else {
+    loadingScreen.classList.add("hidden");
+  }
+};
 
+//Show Details
 
-}
+const showDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
 
+  const res = await fetch(url);
+  const data = await res.json();
+  const singleProduct = data.data;
+  console.log(singleProduct);
+  let productImage = `src='${singleProduct.image}'`
+  let productName = singleProduct.name;
+  let productStorage = singleProduct.mainFeatures.storage;
+  let productChipset = singleProduct.mainFeatures.chipSet;
+  let productMemory = singleProduct.mainFeatures.memory;
+  let productRelease = singleProduct.releaseDate ? singleProduct.releaseDate : "No Data Found" ;
+  let productBrand = singleProduct.brand;
+  let productGPS = singleProduct.others?.GPS ?  singleProduct.others?.GPS : "NO GPS";
+
+  const singleContainer = document.querySelector(".modal-container");
+  singleContainer.innerHTML = "";
+  let singleProductCard = document.createElement("div");
+  singleProductCard.classList = "single-product-card space-y-2";
+  singleProductCard.innerHTML = `
+
+              <div
+                class="flex justify-center items-center bg-slate-200 p-2 rounded-xl w-200"
+              >
+                <img ${productImage} alt="" />
+              </div>
+              <h1 class="text-xl">${productName}</h1>
+              <div class="features flex gap-3 w-full">
+                <div class="features-name font-bold">
+                  <h3>Storage :</h3>
+                  <h3>Chipset :</h3>
+                  <h3>Memory :</h3>
+                  <h3>Release data :</h3>
+                  <h3>Brand :</h3>
+                  <h3>GPS :</h3>
+                </div>
+                <div class="features-details">
+                <h3>${productStorage}</h3>
+                  <h3>${productChipset}</h3>
+                  <h3>${productMemory}</h3>
+                  <h3>${productRelease}</h3>
+                  <h3>${productBrand}</h3>
+                  <h3>${productGPS}</h3>
+                </div>
+              </div>
+         `;
+  singleContainer.appendChild(singleProductCard);
+
+  my_modal_3.showModal();
+};
